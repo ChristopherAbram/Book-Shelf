1ee13348c3412bbd7096f2219d6983dfa6cb99c6
Christopher Abram <krzysztofabram1@gmail.com>
2017-12-12 19:35:00 +0200

    Implementation Parcelable interface for FullUser data object

--- app/src/main/java/com/bookshelf/data/FullUser.java
+++ app/src/main/java/com/bookshelf/data/FullUser.java
@@ -8,7 +8,6 @@ import lombok.Data;
 
 @Data
 public class FullUser implements Parcelable {
-
     private Integer id;
     private String email;
     private String password;
@@ -36,4 +35,77 @@ public class FullUser implements Parcelable {
     private Integer accessLevel;
     private String role;
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.email);
+        dest.writeString(this.password);
+        dest.writeString(this.firstname);
+        dest.writeString(this.lastname);
+        dest.writeString(this.phone);
+        dest.writeValue(this.firstaccess);
+        dest.writeValue(this.lastaccess);
+        dest.writeValue(this.lastlogin);
+        dest.writeValue(this.avatar);
+        dest.writeSerializable(this.sex);
+        dest.writeString(this.cdate);
+        dest.writeString(this.bdate);
+        dest.writeString(this.description);
+        dest.writeValue(this.isactive);
+        dest.writeValue(this.bin);
+        dest.writeString(this.token);
+        dest.writeString(this.country);
+        dest.writeString(this.city);
+        dest.writeString(this.zip);
+        dest.writeString(this.street);
+        dest.writeString(this.house);
+        dest.writeString(this.flat);
+        dest.writeValue(this.accessLevel);
+        dest.writeString(this.role);
+    }
+
+    protected FullUser(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.email = in.readString();
+        this.password = in.readString();
+        this.firstname = in.readString();
+        this.lastname = in.readString();
+        this.phone = in.readString();
+        this.firstaccess = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.lastaccess = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.lastlogin = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.avatar = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.sex = (Character) in.readSerializable();
+        this.cdate = in.readString();
+        this.bdate = in.readString();
+        this.description = in.readString();
+        this.isactive = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.bin = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.token = in.readString();
+        this.country = in.readString();
+        this.city = in.readString();
+        this.zip = in.readString();
+        this.street = in.readString();
+        this.house = in.readString();
+        this.flat = in.readString();
+        this.accessLevel = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.role = in.readString();
+    }
+
+    public static final Parcelable.Creator<FullUser> CREATOR = new Parcelable.Creator<FullUser>() {
+        @Override
+        public FullUser createFromParcel(Parcel source) {
+            return new FullUser(source);
+        }
+
+        @Override
+        public FullUser[] newArray(int size) {
+            return new FullUser[size];
+        }
+    };
 }
